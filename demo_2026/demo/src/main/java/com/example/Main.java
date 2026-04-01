1fa2a7dcf7c90dfd3702c65917fff6b3121ce77c
Claudio <claudio_dj@hotmail.com>
2026-04-01 00:36:20 -0300

    Add files via upload
    Actualización con lo visto el 31/03/26:
    Armado de un array de 5 posiciones

--- demo_2026/demo/src/main/java/com/example/Main.java
+++ demo_2026/demo/src/main/java/com/example/Main.java
@@ -5,36 +5,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Mi nombre es Juan");
-        System.out.println("Tengo 25 años");
-        int edad = 25;
-        String nombre = "Juan";
-        if (edad < 60 && edad > 55) {
-            System.out.println("Estas proximo a jubilarte  " + nombre);
-            
-        }else{
-            System.out.println("Aun no estas proximo a jubilarte " + nombre);
-        }
+        Scanner teclado = new Scanner(System.in);
+        String[] nombres;
+        nombres = new String[5];
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("Dime tu nombre: ");
+            nombres[i] = teclado.nextLine();
+            System.out.println("Dime tu edad: ");
+            int edad = teclado.nextInt();
+            if (edad < 60 && edad > 55) {
+                System.out.println("Estas proximo a jubilarte  " + nombres[i]);
 
-    Scanner teclado = new Scanner(System.in);
-    String teclaPresionada;
-    do{
-    System.out.println("Presione una tecla para continuar (Sale con Z): ");
-    teclaPresionada = teclado.nextLine();
-    switch (teclaPresionada) {
-        case "A":
-            System.out.println("Has presionado la tecla A");
-            break;
-        case "B":
-            System.out.println("Has presionado la tecla B");
-            break;  
-        case "C":
-            System.out.println("Has presionado la tecla C");
-            break;
-        default:
-            System.out.println("Has presionado una tecla no reconocida");
-            break;
-    }
-}while (teclaPresionada.equalsIgnoreCase("Z") == false);
+            } else {
+                System.out.println("Aun no estas proximo a jubilarte " + nombres[i]);
+            }
+            teclado.nextLine(); // Limpiar el buffer después de leer un número
+        }
+        String teclaPresionada;
+        do {
+            System.out.println("Presione una tecla para continuar (Sale con Z): ");
+            teclaPresionada = teclado.nextLine();
+            switch (teclaPresionada) {
+                case "A":
+                case "a":
+                    System.out.println("Has presionado la tecla A");
+                    break;
+                case "B":
+                    System.out.println("Has presionado la tecla B");
+                    break;
+                case "C":
+                    System.out.println("Has presionado la tecla C");
+                    break;
+                default:
+                    System.out.println("Has presionado una tecla no reconocida");
+                    break;
+            }
+        } while (teclaPresionada.equalsIgnoreCase("Z") == false);
     }
 }
