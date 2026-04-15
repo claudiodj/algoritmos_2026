211319cda925e6da031db8a66398b63ec36d24f3
Claudio <claudio_dj@hotmail.com>
2026-04-15 14:55:39 -0300

    Add files via upload
    Clase "Destino" con algunos atributos y los métodos getters y setters

--- turismo/turismo/src/main/java/com/ejemplo/Destinos.java
+++ turismo/turismo/src/main/java/com/ejemplo/Destinos.java
@@ -2,10 +2,40 @@ package com.ejemplo;
 
 public class Destinos {
 
-    String nombre;
+    private String nombre;
     String tipo;   
     int pasajeros;
     boolean disponible;
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public int getPasajeros() {
+        return pasajeros;
+    }
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+    public boolean isDisponible() {
+        return disponible;
+    }
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    @Override
+    public String toString() {
+        return "Destinos [nombre=" + nombre + ", tipo=" + tipo + ", pasajeros=" + pasajeros + ", disponible="
+                + disponible + "]";
+    }
 
     
 
--- turismo/turismo/src/main/java/com/ejemplo/Main.java
+++ turismo/turismo/src/main/java/com/ejemplo/Main.java
@@ -3,5 +3,17 @@ package com.ejemplo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Destinos destino1 = new Destinos();
+        destino1.setNombre("Playa del Carmen");
+        /*
+        destino1.nombre = "Cancún";
+        Al ser el atributo nombre privado, no se puede acceder directamente desde la clase Main, 
+        por lo que se debe utilizar el método setNombre para asignar un valor al atributo nombre del objeto destino1.
+        */
+        destino1.setNombre("Miami");
+
+
+        System.out.println(destino1.getNombre());
     }
 }
