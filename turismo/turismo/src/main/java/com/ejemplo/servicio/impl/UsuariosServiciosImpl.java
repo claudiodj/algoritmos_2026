ff335540260208188ec55395069ea3c3faac9bea
Claudio <claudio_dj@hotmail.com>
2026-09-22 22:18:31 -0300

    Actualizacion clase 22/09/2026
    Se agrego encriptación MD5 a la clave del usuario

--- turismo/turismo/src/main/java/com/ejemplo/servicio/impl/UsuariosServiciosImpl.java
+++ turismo/turismo/src/main/java/com/ejemplo/servicio/impl/UsuariosServiciosImpl.java
@@ -1,5 +1,10 @@
 package com.ejemplo.servicio.impl;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+
 import com.ejemplo.DAO.UsuariosDAO;
 import com.ejemplo.modelo.Usuarios;
 import com.ejemplo.servicio.UsuariosServicios;
@@ -13,13 +18,24 @@ public class UsuariosServiciosImpl implements UsuariosServicios {
         // Verificar si el usuario existe y si la clave coincide
         // adicionalmente verificar que el ID del usuario no sea cero y que el nombre y la clave no sean nulos
         if (usuario.getIdUsuario() != 0 && usuario.getNombreUsuario() != null && usuario.getClaveUsuario() != null) {
-            // Comparar la clave ingresada con la clave almacenada en la base de datos
-            // Se usa equals para comparar cadenas de texto
-            // Si la clave ingresada coincide con la clave almacenada, se retorna TRUE, indicando que el usuario es válido
-            return usuario.getClaveUsuario().equals(claveUsuario);
+            // Comparar la clave ingresada (convertida a MD5) con la clave almacenada en la base de datos
+            String claveMD5 = convertirMD5(claveUsuario);
+            
+            return usuario.getClaveUsuario().equalsIgnoreCase(claveMD5);
         }
         // Si el usuario no existe o la clave no coincide, se retorna FALSE, indicando que el usuario no es válido
         return false;
     }
 
+    private String convertirMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            return HexFormat.of().formatHex(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error al encriptar en MD5", e);
+        }
+    }
+
 }
+
